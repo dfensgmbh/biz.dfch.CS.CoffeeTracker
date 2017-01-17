c7391aba768b398547c80bbc2f165627660bd88f
Link\Steven Pilatschek <steven.pilatschek@gmail.com>
2017-01-17 16:16:00 +0100

    Added Ignored File

---
+++ src/biz.dfch.CS.CoffeeTracker.Core/Release/CommonAssemblySettings.cs
@@ -0,0 +1,32 @@
+﻿/**
+ * Copyright 2017 d-fens GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Web;
+
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("d-fens GmbH")]
+[assembly: AssemblyProduct("biz.dfch.CS.CoffeeTracker.Core")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("The d-fens logo is a registered trademark in the European Union and/or other countries.")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.0.*")]
