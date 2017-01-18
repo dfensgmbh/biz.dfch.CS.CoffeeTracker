305204929bdb470a36a8dcb3a780b72ae3122129
Link\Steven Pilatschek <steven.pilatschek@gmail.com>
2017-01-18 11:26:29 +0100

    Removed CoffeeMachines.cs Added Coffee.cs

--- src/biz.dfch.CS.CoffeeTracker.Core/Model/Coffee.cs
+++ src/biz.dfch.CS.CoffeeTracker.Core/Model/Coffee.cs
@@ -15,25 +15,27 @@
  */
 using System;
 using System.Collections.Generic;
-using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
-using biz.dfch.CS.CoffeeTracker.Core.Model;
 
-namespace biz.dfch.CS.CoffeeTracker.Core.DbContext
+namespace biz.dfch.CS.CoffeeTracker.Core.Model
 {
-    public class CoffeeTrackerDbContext : System.Data.Entity.DbContext
+    public class Coffee : BaseEntity
     {
-        private const string CONNECTION_STRING_NAME = "name=CoffeeTrackerDbContext";
+        [Required]
+        public string Name { get; set; }
 
-        public CoffeeTrackerDbContext() : base(CONNECTION_STRING_NAME)
-        {
-            
-        }
+        [Required]
+        public string Brand { get; set; }
 
-        // Registration of database tables
-        public DbSet<User> Users { get; set; }
-        public DbSet<Statistic> Statistics { get; set; }
-        public DbSet<Coffee> Statistics { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public int Stock { get; set; }
+
+        [Required]
+        public DateTimeOffset LastDelivery { get; set; }
     }
 }
