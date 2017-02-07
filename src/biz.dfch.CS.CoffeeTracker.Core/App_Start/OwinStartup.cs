3516d03d34428ae0cc66964576c243607c68101e
Link\Steven Pilatschek <steven.pilatschek@gmail.com>
2017-02-07 14:24:57 +0100

    Added Cors

--- src/biz.dfch.CS.CoffeeTracker.Core/App_Start/OwinStartup.cs
+++ src/biz.dfch.CS.CoffeeTracker.Core/App_Start/OwinStartup.cs
@@ -30,8 +30,12 @@ namespace biz.dfch.CS.CoffeeTracker.Core.App_Start
     {
         public void Configuration(IAppBuilder app)
         {
-            HttpConfiguration config = new HttpConfiguration();
+            var config = new HttpConfiguration();
+
+            ConfigureOAuth(app);
+
             WebApiConfig.Register(config);
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
             app.UseWebApi(config);
         }
 
