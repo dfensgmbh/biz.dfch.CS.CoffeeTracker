46438f058f2a6014918d4095bb415dffcaa036f7
Link\Steven Pilatschek <steven.pilatschek@gmail.com>
2017-02-07 09:22:50 +0100

    Added Owin Nuget Packages Added Owin Server startup

---
+++ src/biz.dfch.CS.CoffeeTracker.Core/App_Start/OwinStartup.cs
@@ -0,0 +1,36 @@
+﻿/**
+ * Copyright 2017 d-fens GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(biz.dfch.CS.CoffeeTracker.Core.App_Start.OwinStartup))]
+namespace biz.dfch.CS.CoffeeTracker.Core.App_Start
+{
+    public class OwinStartup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            app.UseWebApi(config);
+        }
+    }
+}
