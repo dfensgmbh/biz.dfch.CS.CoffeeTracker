cf86499841900b384c55fd387f1031211f30b570
Link\Steven Pilatschek <steven.pilatschek@gmail.com>
2017-02-07 11:29:30 +0100

    Added AuthorizationManager Added ApplicationUserManager

---
+++ src/biz.dfch.CS.CoffeeTracker.Core/Security/AuthorizationManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics.Contracts;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.ModelBinding;
+using biz.dfch.CS.CoffeeTracker.Core.Logging;
+using biz.dfch.CS.CoffeeTracker.Core.Model;
+using Microsoft.AspNet.Identity;
+
+namespace biz.dfch.CS.CoffeeTracker.Core.Security
+{
+    public class AuthorizationManager
+    {
+        private readonly AuthRepository repository = null;
+
+        public AuthorizationManager()
+        {
+            repository = new AuthRepository();
+        }
+
+        public async Task<IdentityResult> Register(User user)
+        {
+            Contract.Requires(null != user, "|400|");
+            Contract.Requires(user.IsPasswordSafe(), "|400|");
+
+            
+
+            var result = await repository.RegisterUser(user);
+            return result;
+        }
+    }
+}
