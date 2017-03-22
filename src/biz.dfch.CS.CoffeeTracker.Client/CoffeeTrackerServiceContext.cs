9d9c2985e57545daeb484cda2c86f5a068b77822
Link\Steven Pilatschek <steven.pilatschek@gmail.com>
2017-03-22 09:17:28 +0100

    implemented base client added test project added testmethod to verify client sends requests added authenticationhelper class
    resolves #35

---
+++ src/biz.dfch.CS.CoffeeTracker.Client/CoffeeTrackerServiceContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace biz.dfch.CS.CoffeeTracker.Client
+{
+    public class CoffeeTrackerServiceContext
+    {
+        private Uri uri;
+        protected CoffeeTrackerService.Container container;
+
+        public CoffeeTrackerServiceContext(string uri)
+        {
+            this.uri = new Uri(uri);
+            this.container = new CoffeeTrackerService.Container(this.uri);
+
+            container.SendingRequest2 += (s, e) =>
+            {
+                Console.WriteLine("Http Request sent");
+            };
+        }
+
+        public CoffeeTrackerService.Coffee GetCoffee(long id)
+        {
+            Contract.Requires(0 < id);
+
+            var coffee = container.Coffees.Where(c => c.Id == id).FirstOrDefault();
+
+            return coffee;
+        }
+    }
+}
---
+++ src/biz.dfch.CS.CoffeeTracker.Client/biz.dfch.CS.CoffeeTracker.Client.Tests/CoffeeTrackerServiceContextTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace biz.dfch.CS.CoffeeTracker.Client.Tests
+{
+    [TestClass]
+    public class CoffeeTrackerServiceContextTests
+    {
+        private string uri = "http://localhost:49270/api/";
+
+        [TestMethod]
+        public void GetCoffeeSucceeds()
+        {
+            // Arrange
+            // Test assumes coffee is existing
+            var id = 1183;
+
+            // Act
+            var sut = new CoffeeTrackerServiceContext(uri);
+            var coffee = sut.GetCoffee(id);
+
+            // Assert
+            Assert.IsNotNull(coffee);
+            Assert.AreEqual(id, coffee.Id);
+        }
+    }
+}
