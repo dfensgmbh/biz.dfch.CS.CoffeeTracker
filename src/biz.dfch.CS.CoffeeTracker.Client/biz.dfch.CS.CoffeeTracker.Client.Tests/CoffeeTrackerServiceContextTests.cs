ca67a458d6088d6a0b188af1c180def75cff345e
Link\Steven Pilatschek <steven.pilatschek@gmail.com>
2017-03-28 09:31:51 +0200

    Added test to verify the client can register and use the api
    resolves #37

--- src/biz.dfch.CS.CoffeeTracker.Client/biz.dfch.CS.CoffeeTracker.Client.Tests/CoffeeTrackerServiceContextTests.cs
+++ src/biz.dfch.CS.CoffeeTracker.Client/biz.dfch.CS.CoffeeTracker.Client.Tests/CoffeeTrackerServiceContextTests.cs
@@ -4,6 +4,7 @@ using System.Net;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using biz.dfch.CS.Testing.Attributes;
+using Microsoft.Data.OData;
 
 namespace biz.dfch.CS.CoffeeTracker.Client.Tests
 {
@@ -16,23 +17,6 @@ namespace biz.dfch.CS.CoffeeTracker.Client.Tests
         private const string INVALID_USERNAME = "notAnEmail";
         private const string INVALID_PASSWORD = "0000";
 
-        [TestMethod]
-        public void GetCoffeeSucceeds()
-        {
-            // Arrange
-            // Test assumes coffee is existing
-            var id = 1183;
-
-            // Act
-            var sut = new CoffeeTrackerServiceContext(uri, adminUserName, adminPassword);
-            // oData Client can't resolve FirstOrDefault() directly
-            var result = sut.container.Coffees.Where(c => c.Id == id).FirstOrDefault();
-
-            // Assert
-            Assert.IsNotNull(sut);
-            Assert.IsNotNull(result);
-            Assert.AreEqual(id, result.Id);
-        }
 
         [TestMethod]
         [ExpectedException(typeof(Exception), "token")]
@@ -56,6 +40,55 @@ namespace biz.dfch.CS.CoffeeTracker.Client.Tests
 
             // Act / Assert
             await sut.authenticationHelper.ReceiveAndSetToken(INVALID_USERNAME, INVALID_PASSWORD);
+        }
+
+        // If this test works, the client is able to send CRUD requests
+        [TestMethod]
+        public void CreateReadUpdateAndDeleteUser()
+        {
+            // Arrange
+            var userName = string.Format("ClientIntegrationTest-{0}@Example.com", Guid.NewGuid());
+            var newUserName = string.Format("ClientIntegrationTest-{0}@Example.com", Guid.NewGuid());
+            var password = "123456";
+
+            var user = new CoffeeTrackerService.ApplicationUser
+            {
+                Name = userName,
+                Password = password
+            };
+
+            // Act
+
+            // / Create
+            var sut = new CoffeeTrackerServiceContext(uri);
+            Assert.IsNotNull(sut);
+
+            sut.container.AddToUsers(user);
+            sut.container.SaveChanges();
+
+            // Login into the api
+            sut.authenticationHelper.ReceiveAndSetToken(userName, password).Wait();
+
+            // / Read
+            // oData Client can't resolve FirstOrDefault() directly
+            var createdUserFromDb = sut.container.Users.Where(u => u.Name == userName).FirstOrDefault();
+            Assert.IsNotNull(createdUserFromDb);
+
+            // / Update
+            createdUserFromDb.Name = newUserName;
+            sut.container.SaveChanges();
+
+            // oData Client can't resolve FirstOrDefault() directly
+            createdUserFromDb = sut.container.Users.Where(u => u.Name == userName).FirstOrDefault();
+            Assert.IsNotNull(createdUserFromDb);
+            Assert.AreEqual(newUserName, createdUserFromDb.Name);
+
+            // / Delete
+            sut.container.DeleteObject(createdUserFromDb);
+            sut.container.SaveChanges();
+
+            var userShouldBeNull = sut.container.Users.Where(u => u.Name == newUserName);
+            Assert.IsNull(userShouldBeNull);
         }
     }
 }
