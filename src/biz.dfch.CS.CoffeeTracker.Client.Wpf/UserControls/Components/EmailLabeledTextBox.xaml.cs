834e86432839b06053fdefe796228d4ab49720da
Link\Steven Pilatschek <steven.pilatschek@gmail.com>
2017-04-11 10:57:11 +0200

    added property

--- src/biz.dfch.CS.CoffeeTracker.Client.Wpf/UserControls/Components/EmailLabeledTextBox.xaml.cs
+++ src/biz.dfch.CS.CoffeeTracker.Client.Wpf/UserControls/Components/EmailLabeledTextBox.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,11 +22,29 @@ namespace biz.dfch.CS.CoffeeTracker.Client.Wpf.UserControls.Components
     public partial class EmailLabeledTextBox : IValidatable
     {
         private Brush oldBorderBrush;
+        public static readonly DependencyProperty ToolTipContentProperty = DependencyProperty.Register("ToolTipContent", typeof(string), typeof(EmailLabeledTextBox));
+
+        public string ToolTipContent
+        {
+            get
+            {
+                return GetValue(ToolTipContentProperty) as string;
+            }
+            set
+            {
+                SetValue(ToolTipContentProperty, value);
+            }
+        }
 
         public EmailLabeledTextBox()
         {
             InitializeComponent();
             oldBorderBrush = EmailTextBox.BorderBrush;
+            var dpd = DependencyPropertyDescriptor.FromProperty(ToolTipContentProperty, typeof(EmailLabeledTextBox));
+            dpd.AddValueChanged(this, (sender, args) =>
+            {
+                EmailTextBox.ToolTip = this.ToolTipContent;
+            });
         }
 
         public bool Validate()
