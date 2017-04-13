03d4fa90710a758cabae3ba491b4ee4a81602239
Link\Steven Pilatschek <steven.pilatschek@gmail.com>
2017-04-13 12:32:35 +0200

    implemented coffeeOrdering

--- src/biz.dfch.CS.CoffeeTracker.Client.Wpf/UserControls/CompleteViews/Base/Home.xaml.cs
+++ src/biz.dfch.CS.CoffeeTracker.Client.Wpf/UserControls/CompleteViews/Base/Home.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,8 @@ namespace biz.dfch.CS.CoffeeTracker.Client.Wpf.UserControls.CompleteViews.Base
     /// </summary>
     public partial class Home : UserControl
     {
+        private Coffee selectedCoffee;
+
         public Home()
         {
             InitializeComponent();
@@ -33,15 +36,48 @@ namespace biz.dfch.CS.CoffeeTracker.Client.Wpf.UserControls.CompleteViews.Base
             HomePriceLabel.Content = e.SelectedCoffee.Price;
             HomeOnStockLabel.Content = e.SelectedCoffee.Stock;
             HomeOrderButton.IsEnabled = true;
+            selectedCoffee = e.SelectedCoffee;
         }
 
         private void HomeOrderButton_OnClick(object sender, RoutedEventArgs e)
         {
+            var worker = new BackgroundWorker();
+            DisplayLoading();
+            worker.DoWork += (o, args) =>
+            {
+                AddCoffeeOrder();
+            };
+            worker.RunWorkerCompleted += (o, args) =>
+            {
+                HideLoading();
+            };
+
+            worker.RunWorkerAsync();
+        }
+
+        private void AddCoffeeOrder()
+        {
             var client = ClientContext.GetServiceContext();
             var coffeeOrder = new CoffeeOrder()
             {
-                
+                Name = ClientContext.CurrentUserName+DateTimeOffset.Now,
+                UserId = ClientContext.CurrentUserId,
+                CoffeeId = selectedCoffee.Id
             };
+            client.AddToCoffeeOrders(coffeeOrder);
+            client.SaveChanges();
+        }
+
+        private void DisplayLoading()
+        {
+            HomeAddOrderViewBox.Visibility = Visibility.Collapsed;
+            HomeAddOrderGridLoading.Visibility = Visibility.Visible;
+        }
+
+        private void HideLoading()
+        {
+            HomeAddOrderViewBox.Visibility = Visibility.Visible;
+            HomeAddOrderGridLoading.Visibility = Visibility.Collapsed;
         }
     }
 }
