074d2c037a8c63b14d2a12055e161c7634a08934
Link\Steven Pilatschek <steven.pilatschek@gmail.com>
2017-04-18 14:22:57 +0200

    added TestMethod and TestClass

---
+++ src/biz.dfch.CS.CoffeeTracker.Client.Wpf.Tests/Managers/LoginManagerTests.cs
@@ -0,0 +1,50 @@
+﻿/**
+ * Copyright 2017 d-fens GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using biz.dfch.CS.CoffeeTracker.Client.Wpf.Managers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Telerik.JustMock;
+
+namespace biz.dfch.CS.CoffeeTracker.Client.Wpf.Tests.Managers
+{
+    [TestClass]
+    public class LoginManagerTests
+    {
+        private const string EXAMPLEBEARER_TOKEN = "EXAMPLETOKENmZ1edKKACtPAb7zGlwSzvs72PvhAbGmB8K1ZrGxpcNM";
+
+        [TestMethod]
+        public async Task LoginManagerLoginSucceeds()
+        {
+            // Arrange
+            var fakeReference = Mock.Create<CoffeeTrackerServiceContext>();
+            Mock.Arrange(
+                () =>
+                    fakeReference.authenticationHelper.ReceiveAndSetToken(SharedTestData.UserWhichExists,
+                        SharedTestData.PasswordForUserWhichExists)).MustBeCalled("ReceiveAndSetToken was not called");
+
+            // Act
+            var sut = new LoginManager(fakeReference);
+            await sut.Login(SharedTestData.UserWhichExists, SharedTestData.UserNameWhichShouldNotExist);
+
+            // Assert
+            Mock.Assert(fakeReference);
+        }
+    }
+}
