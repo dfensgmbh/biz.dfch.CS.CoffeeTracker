f61ef63eb6a2a0418d443cc958af04f83f193c02
Link\Steven Pilatschek <steven.pilatschek@gmail.com>
2017-04-24 12:23:48 +0200

    added onClick event

--- src/biz.dfch.CS.CoffeeTracker.Client.Wpf/UserInterface/UserControls/CompleteViews/Base/Coffees.xaml.cs
+++ src/biz.dfch.CS.CoffeeTracker.Client.Wpf/UserInterface/UserControls/CompleteViews/Base/Coffees.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows.Controls;
+using biz.dfch.CS.CoffeeTracker.Client.CoffeeTrackerService;
+using biz.dfch.CS.CoffeeTracker.Client.Wpf.Classes.Managers;
+using biz.dfch.CS.CoffeeTracker.Client.Wpf.Controls;
 
 namespace biz.dfch.CS.CoffeeTracker.Client.Wpf.UserControls.CompleteViews.Base
 {
@@ -7,9 +11,28 @@ namespace biz.dfch.CS.CoffeeTracker.Client.Wpf.UserControls.CompleteViews.Base
     /// </summary>
     public partial class Coffees : UserControl
     {
+        private CoffeesManager manager = new CoffeesManager(ClientContext.CoffeeTrackerServiceContext);
+
         public Coffees()
         {
             InitializeComponent();
+        }
+
+        public void AddButton_OnClick(object sender, EventArgs args)
+        {
+            var isValid = CoffeeCoffeeForm.IsValid();
+            if (isValid)
+            {
+                var newCoffee = new Coffee
+                {
+                    Name = CoffeeCoffeeForm.CoffeeFormNameTextBox.Text,
+                    Brand = CoffeeCoffeeForm.CoffeeFormBrandTextBox.Text,
+                    Price = decimal.Parse(CoffeeCoffeeForm.CoffeeFormPriceTextBox.Text),
+                    Stock = Int32.Parse(CoffeeCoffeeForm.CoffeeFormStockTextBox.Text),
+                    LastDelivery = DateTimeOffset.Parse(CoffeeCoffeeForm.CoffeeFormLastDeliveryTextBox.Text)
+                };
+                manager.AddCoffee(newCoffee);
+            }
         }
     }
 }
